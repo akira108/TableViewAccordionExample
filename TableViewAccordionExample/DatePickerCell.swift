15892798847db36d787d2f5e27e789f7cc1658a8
Akira Iwaya <108.akira.108@gmail.com>
2016-02-02 20:33:01 +0900

    Implement Accordion

---
+++ TableViewAccordionExample/DatePickerCell.swift
@@ -0,0 +1,24 @@
+//
+//  DatePickerCell.swift
+//  TableViewAccordionExample
+//
+//  Created by Akira Iwaya on 2016/02/02.
+//  Copyright © 2016年 akira108. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
