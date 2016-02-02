71d19d6286eb2a8d4dd5224966fcbdf77d7ff5f0
Akira Iwaya <108.akira.108@gmail.com>
2016-02-02 20:09:05 +0900

    Initial Commit

---
+++ TableViewAccordionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewAccordionExample
+//
+//  Created by Akira Iwaya on 2016/02/02.
+//  Copyright © 2016年 akira108. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
